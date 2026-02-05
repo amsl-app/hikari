5a9986da0dd0e48c6ecc9c566bcf4dfcc8d59ad1
Linus Bierhoff <accounts@linusbierhoff.com>
2026-02-05 11:51:22 +0100

    adapt run scripts

--- crates/hikari-utils/src/args/tts.rs
+++ crates/hikari-utils/src/args/tts.rs
@@ -6,11 +6,11 @@ use crate::loader::s3::S3Config;
 #[derive(Debug, Clone, Args)]
 #[allow(clippy::struct_field_names)]
 pub struct TTSConfig {
-    #[arg(long = "elevenlabs_key", required = false)]
+    #[arg(long = "elevenlabs-key", required = false)]
     pub api_key: String,
-    #[arg(long = "elevenlabs_model", required = false)]
+    #[arg(long = "elevenlabs-model", required = false)]
     pub model: String,
-    #[arg(long = "elevenlabs_voice", required = false)]
+    #[arg(long = "elevenlabs-voice", required = false)]
     pub voice: String,
     #[command(flatten)]
     pub cache_config: Option<TTSCacheConfig>,
@@ -19,13 +19,13 @@ pub struct TTSConfig {
 #[derive(Debug, Clone, Args)]
 #[allow(clippy::struct_field_names)]
 pub struct TTSCacheConfig {
-    #[arg(long = "cache_s3_endpoint", required = false)]
+    #[arg(long = "cache-s3-endpoint", required = false)]
     pub endpoint: Url,
-    #[arg(long = "cache_s3_region", required = false)]
+    #[arg(long = "cache-s3-region", required = false)]
     pub region: String,
-    #[arg(long = "cache_s3_access_key", required = false)]
+    #[arg(long = "cache-s3-access_key", required = false)]
     pub access_key: String,
-    #[arg(long = "cache_s3_secret_key", required = false)]
+    #[arg(long = "cache-s3-secret_key", required = false)]
     pub secret_key: String,
 }
 
