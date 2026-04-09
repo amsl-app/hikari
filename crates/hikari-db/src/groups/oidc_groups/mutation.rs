67f62b2d23fc28782721e45de412ad4b874e652d
Linus Bierhoff <accounts@linusbierhoff.com>
2026-04-09 11:44:51 +0200

    update on conflict

--- crates/hikari-db/src/groups/oidc_groups/mutation.rs
+++ crates/hikari-db/src/groups/oidc_groups/mutation.rs
@@ -1,5 +1,6 @@
 use hikari_entity::oidc_groups;
 use hikari_entity::oidc_groups::{ActiveModel, Entity};
+use sea_orm::sea_query::OnConflict;
 use std::collections::HashSet;
 
 use crate::util::FlattenTransactionResultExt;
@@ -23,6 +24,10 @@ impl Mutation {
             })
             .collect::<Vec<_>>();
 
+        let on_conflict = OnConflict::columns([oidc_groups::Column::UserId, oidc_groups::Column::Value])
+            .update_column(oidc_groups::Column::Value)
+            .to_owned();
+
         db.transaction(|conn| {
             Box::pin(async move {
                 Entity::delete_many()
@@ -30,7 +35,7 @@ impl Mutation {
                     .exec(conn)
                     .await?;
                 if !groups.is_empty() {
-                    Entity::insert_many(groups).on_conflict_do_nothing().exec(conn).await?;
+                    Entity::insert_many(groups).on_conflict(on_conflict).exec(conn).await?;
                 }
                 Result::<_, DbErr>::Ok(())
             })
