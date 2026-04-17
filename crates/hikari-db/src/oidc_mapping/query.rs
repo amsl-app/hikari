c00041a9c01a9a33deb4fe4b4aed2be641c3270b
Linus Bierhoff <accounts@linusbierhoff.com>
2026-04-17 14:20:02 +0200

    check for user before recreating

--- crates/hikari-db/src/oidc_mapping/query.rs
+++ crates/hikari-db/src/oidc_mapping/query.rs
@@ -1,10 +1,20 @@
 use hikari_entity::oidc_mapping::{Entity, Model};
-use sea_orm::{DbConn, DbErr, EntityTrait};
+use sea_orm::{ColumnTrait, ConnectionTrait, DbConn, DbErr, EntityTrait, QueryFilter, TransactionTrait};
 
 pub struct Query;
 
 impl Query {
     pub async fn find_by_id(db: &DbConn, id: i32) -> Result<Option<Model>, DbErr> {
         Entity::find_by_id(id).one(db).await
+    }
+
+    pub async fn find_for_sub<C: ConnectionTrait + TransactionTrait>(
+        db: &C,
+        sub: &str,
+    ) -> Result<Option<Model>, DbErr> {
+        Entity::find()
+            .filter(hikari_entity::oidc_mapping::Column::OidcSub.eq(sub.to_string()))
+            .one(db)
+            .await
     }
 }
--- crates/hikari-server/src/db/sea_orm/user.rs
+++ crates/hikari-server/src/db/sea_orm/user.rs
@@ -3,67 +3,83 @@ use hikari_entity::user::Model as User;
 use sea_orm::prelude::*;
 use sea_orm::{TransactionError, TransactionTrait};
 use std::collections::HashSet;
-use thiserror::Error;
 
-#[derive(Debug, Error)]
-enum UserCreationError {
-    #[error(transparent)]
-    DbErr(#[from] DbErr),
-
-    #[error("User exists")]
-    UserExists { user_id: Uuid },
+pub async fn check_for_user_id<C: ConnectionTrait + TransactionTrait>(
+    conn: &C,
+    sub: &str,
+) -> Result<Option<Uuid>, DbErr> {
+    let mapping = hikari_db::oidc_mapping::Query::find_for_sub(conn, sub).await?;
+    Ok(mapping.map(|m| m.user_id))
 }
 
-pub async fn create_user_id<C: ConnectionTrait + TransactionTrait>(conn: &C, sub: &str) -> Result<Uuid, DbErr> {
-    let sub = sub.to_string();
+pub async fn update_oidc_groups<C: ConnectionTrait + TransactionTrait>(
+    conn: &C,
+    user_id: Uuid,
+    groups: &HashSet<String>,
+) -> Result<(), DbErr> {
+    let current_groups = hikari_db::groups::oidc_groups::Query::get_for_user(conn, user_id).await?;
+    let current_group_names: HashSet<String> = current_groups.into_iter().map(|g| g.value).collect();
+    if &current_group_names != groups {
+        hikari_db::groups::oidc_groups::Mutation::set(conn, user_id, groups.clone()).await?;
+    }
+    Ok(())
+}
 
-    let res = conn
-        .transaction(|txn| {
-            Box::pin(async move {
-                let user = hikari_db::user::Mutation::create_user(txn).await?;
-                let mapping = hikari_db::oidc_mapping::Mutation::create_oidc_mapping(txn, user.id, sub).await?;
-                if user.id.eq(&mapping.user_id) {
-                    Result::<_, UserCreationError>::Ok(user.id)
-                } else {
-                    Err(UserCreationError::UserExists {
-                        user_id: mapping.user_id,
-                    })
-                }
-            })
-        })
-        .await;
-    let user_id = match res {
-        Ok(user_id) | Err(TransactionError::Transaction(UserCreationError::UserExists { user_id })) => user_id,
-        Err(TransactionError::Connection(error) | TransactionError::Transaction(UserCreationError::DbErr(error))) => {
-            return Err(error);
-        }
-    };
-    Ok(user_id)
+pub async fn get_user<C: ConnectionTrait + TransactionTrait>(
+    conn: &C,
+    sub: &str,
+    groups: &HashSet<String>,
+) -> Result<Option<(User, Vec<String>)>, DbErr> {
+    let sub = sub.to_string();
+    let user_id = check_for_user_id(conn, &sub).await?;
+    if let Some(user_id) = user_id {
+        update_oidc_groups(conn, user_id, groups).await?;
+        let user = hikari_db::user::Query::find_user_by_id(conn, user_id)
+            .await?
+            .ok_or_else(|| DbErr::RecordNotFound("Record not found after checking for user id".to_owned()))?;
+        let custom_groups = hikari_db::groups::custom_groups::Query::get_for_user(conn, user_id).await?;
+        return Ok(Some((user, custom_groups)));
+    }
+    Ok(None)
 }
 
 pub async fn create_user<C: ConnectionTrait + TransactionTrait>(
     conn: &C,
     sub: &str,
     groups: HashSet<String>,
-) -> Result<(User, Vec<String>), DbErr> {
+) -> Result<User, DbErr> {
     let sub = sub.to_string();
-
-    conn.transaction(|txn| {
-        Box::pin(async move {
-            let user_id = create_user_id(txn, sub.as_str()).await?;
-            let user = hikari_db::user::Query::find_user_by_id(txn, user_id)
-                .await?
-                .ok_or_else(|| DbErr::RecordNotFound("Record not found after insertion".to_owned()))?;
-            hikari_db::groups::oidc_groups::Mutation::set(txn, user.id, groups).await?;
-            let custom_groups = hikari_db::groups::custom_groups::Query::get_for_user(txn, user_id).await?;
-            Ok((user, custom_groups))
+    let user = conn
+        .transaction(|txn| {
+            Box::pin(async move {
+                let user = hikari_db::user::Mutation::create_user(txn).await?;
+                hikari_db::oidc_mapping::Mutation::create_oidc_mapping(txn, user.id, sub).await?;
+                hikari_db::groups::oidc_groups::Mutation::set(txn, user.id, groups).await?;
+                Ok(user)
+            })
         })
-    })
-    .await
-    .flatten_res()
+        .await;
+    let user = match user {
+        Ok(user) => user,
+        Err(TransactionError::Transaction(err)) | Err(TransactionError::Connection(err)) => return Err(err),
+    };
+    Ok(user)
 }
 
-pub async fn create_user_and_get_token<C: ConnectionTrait + TransactionTrait>(
+pub async fn get_or_create_user<C: ConnectionTrait + TransactionTrait>(
+    conn: &C,
+    sub: &str,
+    groups: &HashSet<String>,
+) -> Result<(User, Vec<String>), DbErr> {
+    if let Some((user, custom_groups)) = get_user(conn, sub, groups).await? {
+        Ok((user, custom_groups))
+    } else {
+        let user = create_user(conn, sub, groups.clone()).await?;
+        Ok((user, Vec::new()))
+    }
+}
+
+pub async fn get_or_create_user_and_get_token<C: ConnectionTrait + TransactionTrait>(
     conn: &C,
     sub: &str,
     groups: HashSet<String>,
@@ -72,10 +88,9 @@ pub async fn create_user_and_get_token<C: ConnectionTrait + TransactionTrait>(
 
     conn.transaction(|txn| {
         Box::pin(async move {
-            let user_id = create_user_id(txn, &sub).await?;
-            let token = hikari_db::access_tokens::Mutation::create_access_token(txn, user_id).await?;
+            let (user, _) = get_or_create_user(txn, &sub, &groups).await?;
+            let token = hikari_db::access_tokens::Mutation::create_access_token(txn, user.id).await?;
 
-            hikari_db::groups::oidc_groups::Mutation::set(txn, user_id, groups).await?;
             Ok(token)
         })
     })
