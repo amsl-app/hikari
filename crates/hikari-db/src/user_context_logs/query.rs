9b14f2f2d6b57cfe7a9a4a4c6fd0f00d584c02c3
Linus Bierhoff <accounts@linusbierhoff.com>
2026-05-20 11:46:02 +0200

    add query from user_context_log

--- crates/hikari-db/src/user_context_logs/query.rs
+++ crates/hikari-db/src/user_context_logs/query.rs
@@ -21,6 +21,14 @@ impl Query {
             .await
     }
 
+    pub async fn get_earliest<C: ConnectionTrait>(conn: &C, user_id: Uuid) -> Result<Option<Model>, DbErr> {
+        Entity::find()
+            .filter(Column::UserId.eq(user_id))
+            .order_by(Column::CreatedAt, Order::Asc)
+            .one(conn)
+            .await
+    }
+
     pub async fn get_all_by_type<C: ConnectionTrait>(
         conn: &C,
         user_id: Uuid,
@@ -43,6 +51,19 @@ impl Query {
             .filter(Column::UserId.eq(user_id))
             .filter(Column::Type.eq(log_type))
             .order_by(Column::CreatedAt, Order::Desc)
+            .one(conn)
+            .await
+    }
+
+    pub async fn get_earliest_by_type<C: ConnectionTrait>(
+        conn: &C,
+        user_id: Uuid,
+        log_type: &str,
+    ) -> Result<Option<Model>, DbErr> {
+        Entity::find()
+            .filter(Column::UserId.eq(user_id))
+            .filter(Column::Type.eq(log_type))
+            .order_by(Column::CreatedAt, Order::Asc)
             .one(conn)
             .await
     }
