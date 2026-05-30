a56f67434ecb984b6aa167a7ef97c53c984c557c
Sven Scheu <itssvens@gmail.com>
2026-05-30 11:18:28 +0200

    Remove useless clones

--- crates/hikari-llm/src/execution/steps/conversation_validator.rs
+++ crates/hikari-llm/src/execution/steps/conversation_validator.rs
@@ -123,12 +123,12 @@ impl LlmStepTrait for ConversationValidator {
             };
 
             let goto = if success {
-                self.goto_on_success.clone()
+                self.goto_on_success.as_ref()
             } else {
-                self.goto_on_fail.clone()
+                self.goto_on_fail.as_ref()
             };
 
-            let goto = resolve_optional(goto.as_ref(), conversation_id, user_id, module_id, session_id, &conn).await?;
+            let goto = resolve_optional(goto, conversation_id, user_id, module_id, session_id, &conn).await?;
             let next_step = goto.map(super::template_to_step_id).transpose()?;
 
             let content = LlmStepContent::StepValue {
--- crates/hikari-llm/src/execution/steps/value_extractor.rs
+++ crates/hikari-llm/src/execution/steps/value_extractor.rs
@@ -122,13 +122,13 @@ impl LlmStepTrait for ValueExtractor {
                 }
 
                 let goto = if success {
-                    self.goto_on_success.clone()
+                    self.goto_on_success.as_ref()
                 } else {
-                    self.goto_on_fail.clone()
+                    self.goto_on_fail.as_ref()
                 };
 
                 let goto =
-                    resolve_optional(goto.as_ref(), conversation_id, user_id, module_id, session_id, &conn).await?;
+                    resolve_optional(goto, conversation_id, user_id, module_id, session_id, &conn).await?;
                 let next_step = goto.map(super::template_to_step_id).transpose()?;
 
                 Ok(LlmStepResponse::new(
