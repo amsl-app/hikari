b0ae0d5645c37686140236839f64d496c60b4c1a
Linus Bierhoff <accounts@linusbierhoff.com>
2026-08-14 22:44:41 +0200

    use snake_case for next session

--- crates/hikari-model/src/module/session.rs
+++ crates/hikari-model/src/module/session.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 
 use crate::module::{
     locked_until,
+    next_session::Next,
     session::instance::{SessionInstance, SessionInstanceStatus},
 };
 use chrono::{DateTime, Utc};
@@ -10,7 +11,6 @@ use hikari_config::{
     module::{
         content::{Content, ContentSource},
         llm_agent::LlmService,
-        next_session::Next,
         session::Session,
         unlock::{LockedUntil, Unlock},
     },
@@ -79,7 +79,7 @@ pub struct SessionFull<'a> {
     pub next_session: Option<&'a str>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub next: Option<&'a Next>,
+    pub next: Option<Next>,
 
     pub status: SessionInstanceStatus,
 
@@ -151,7 +151,7 @@ impl<'a> SessionFull<'a> {
             banner: session.banner.as_deref(),
             theme: session.theme.as_ref(),
             time: session.time.as_ref(),
-            next: session.next.as_ref(),
+            next: session.next.as_ref().map(Next::from_config),
             #[allow(deprecated)]
             next_session: session.next_session.as_deref(),
             unlock: session.unlock.as_ref(),
